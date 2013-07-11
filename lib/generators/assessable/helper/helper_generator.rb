3db54ef02cc17560f744f038266c77382fa27714
Steve Alex <salex@mac.com>
2013-07-11 02:09:40 -0500

    Bump version. Correct display_helper installer

--- lib/generators/assessable/helper/helper_generator.rb
+++ lib/generators/assessable/helper/helper_generator.rb
@@ -4,7 +4,7 @@ module Assessable
       desc "This generator creates full helper"
       source_root File.expand_path('../templates', __FILE__)
       def create_helper_file
-        copy_file "take_helper.rb", "app/helpers/assessable/take_helper.rb"
+        copy_file "display_helper.rb", "app/helpers/assessable/display_helper.rb"
       end
       
     end
--- lib/generators/assessable/install/install_generator.rb
+++ lib/generators/assessable/install/install_generator.rb
@@ -11,8 +11,8 @@ module Assessable
         copy_file "assessing.rb", "app/models/assessing.rb"
       end
       
-      def create_taking_file
-        copy_file "take_helper.rb", "app/helpers/assessable/take_helper.rb"
+      def create_display_helper_file
+        copy_file "display_helper.rb", "app/helpers/assessable/display_helper.rb"
       end
       
       def create_take_js_file
